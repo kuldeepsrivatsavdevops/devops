0d9bc8f95da7250db4d1c0cbd1ea6e4f5c6c0824
Kuldeep Srivatsav <kuldeepsrivatsav1@gmail.com>
2026-07-19 21:33:24 -0400

    Fix quotes in AWS instance configuration

--- praticeterraform.tf
+++ praticeterraform.tf
@@ -2,8 +2,8 @@ provider "aws"{
     region "ca-central-1"
 }
 resource "aws_instance" "first"{
-    ami = “ami-0d3edbd2903961ec9”
-    instance_type = “t3.micro”
+    ami = "ami-0d3edbd2903961ec9"
+    instance_type = "t3.micro"
     count = 2
 }
 tags = {
