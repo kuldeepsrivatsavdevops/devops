85f6db1fa736ee3c4c36e7c44ade4c09c674b644
Kuldeep Srivatsav <kuldeepsrivatsav1@gmail.com>
2026-07-19 21:46:09 -0400

    Rename S3 bucket to 'dev1bucket'
    Updated S3 bucket name to match naming conventions.

--- praticeterraform.tf
+++ praticeterraform.tf
@@ -1,12 +1,7 @@
 provider "aws"{
     region = "ca-central-1"
 }
-resource "aws_instance" "first"{
-    ami = "ami-0d3edbd2903961ec9"
-    instance_type = "t3.micro"
-    count = 2
-}
 
 resource "aws_s3_bucket" "firstbucket"{
-    bucket = "dev1Bucket"
+    bucket = "dev1bucket"
 }
