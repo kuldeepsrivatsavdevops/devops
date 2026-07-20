29da6a25761db61220b936a7d0de4150c9194aa8
Kuldeep Srivatsav <kuldeepsrivatsav1@gmail.com>
2026-07-19 22:05:47 -0400

    Rename S3 bucket and resource name

--- praticeterraform.tf
+++ praticeterraform.tf
@@ -1,6 +1,6 @@
 provider "aws"{
     region = "us-east-2"
 }
-resource "aws_s3_bucket" "buckt"{
-    bucket = "gilmorebucket"
+resource "aws_s3_bucket" "bucket"{
+    bucket = "dragonlordsbucket"
 }
