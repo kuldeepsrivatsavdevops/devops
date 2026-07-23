a014e82b48b38574ca272d97134635a2b18e3d02
Kuldeep Srivatsav <kuldeepsrivatsav1@gmail.com>
2026-07-22 21:05:49 -0400

    /

--- ec2.tf
+++ ec2.tf
@@ -5,9 +5,9 @@ resource "aws_instance" "first" {
     ami = "ami-078fe7ff43e10cf8c"
     instance_type = "t3.micro"
     count = 2
-}
-tags = {
+    tags = {
     Name = "Dev1"
+}
 }
 resource "aws_s3_bucket" "firstbucket" {
     bucket = "lordBucket"
